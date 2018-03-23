477f8f4c8ea04bf019f9d04e68654223c05e28c5
SilverLaius <silver.laius@gmail.com>
2018-03-23 15:35:51 +0200

    gitignore+Map class+package rename

---
+++ src/server/Map.java
@@ -0,0 +1,16 @@
+package server;
+
+public class Map {
+
+    public static char[][] generateMap(int size) {
+        char[][] map = new char[size][size];
+        // get fillet bby
+
+        return map;
+    }
+
+    public static char[][] reduceMapSize(char[][] map) {
+        // magic happens
+        return map;
+    }
+}
