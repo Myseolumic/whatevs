cbdb4105834189c0e904ff0f2816b7927ddd1bfc
greksn <34744479+greksn@users.noreply.github.com>
2018-04-04 20:50:11 +0300

    töötab v?

---
+++ src/main/java/main/Server.java
@@ -0,0 +1,27 @@
+package main;
+
+import java.io.Serializable;
+import java.util.function.Consumer;
+
+public class Server extends NetworkConnection {
+    Server(int port, Consumer<Serializable> onReceiveCallback) {
+        super(onReceiveCallback);
+        this.port = port;
+    }
+    private int port;
+
+    @Override
+    protected boolean isServer() {
+        return true;
+    }
+
+    @Override
+    protected String getIP() {
+        return null;
+    }
+
+    @Override
+    protected int getPort() {
+        return port;
+    }
+}
