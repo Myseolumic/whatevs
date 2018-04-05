f41724b7120d28ffd6bd8137f66df1e2988bac59
Myseolumic <karl32123@gmail.com>
2018-04-05 11:21:58 +0300

    Revert "mdea kas sul on osad klassid samad , nagu Server ja Client,aga loodame et perse ei lähe :D"
    This reverts commit b30f1182cdbda1a47ffc4be3f8dc4efb14181ff1.

--- src/main/java/main/Main.java
+++ src/main/java/main/Main.java
@@ -1,47 +1,15 @@
 package main;
 
 import javafx.application.Application;
-import javafx.application.Platform;
 import javafx.scene.Scene;
-import javafx.scene.control.TextArea;
-import javafx.scene.control.TextField;
 import javafx.scene.layout.BorderPane;
 import javafx.scene.layout.GridPane;
-import javafx.scene.layout.VBox;
 import javafx.stage.Stage;
 import server.Map;
 import server.Tile;
 
 
 public class Main extends Application {
-    private TextArea messages = new TextArea();
-    private boolean isServer = false;
-    private NetworkConnection connection = isServer ? createServer(): createClient();
-
-    @Override
-    public void init() throws Exception {
-        connection.startConnection();
-    }
-
-    public void stop() throws Exception{
-        connection.closeConnection();
-    }
-
-    private Server createServer(){
-        return new Server(1337, data ->{
-            Platform.runLater(() -> {
-                messages.appendText(data.toString() + "\n");
-            });
-        });
-    }
-
-    private Client createClient(){
-        return new Client("127.0.0.1",1337,data ->{
-            Platform.runLater(() -> {
-                messages.appendText(data.toString() + "\n");
-            });
-        });
-    }
 
     @Override
     public void start(Stage primaryStage) throws Exception {
@@ -51,28 +19,8 @@ public class Main extends Application {
         Map.reduceMapSize(miniMap);
         Map.visualizeMap(map, miniMap);
 
-        ////////////////////////////////////
-        messages.setPrefHeight(600);
-        TextField input = new TextField();
-        input.setOnAction(event -> {
-            String message = isServer ? "Server: " : "Client: ";
-            message += input.getText();
-            input.clear();
-
-            messages.appendText(message + "\n");
-
-            try {
-                connection.send(message);
-            } catch (Exception e) {
-                messages.appendText("Exeption happened\n");
-            }
-        });
-        VBox vBox = new VBox(20,messages,input);
-        vBox.setPrefSize(60,300);
-        ////////////////////////////////////////////////////7
 
         client.setCenter(map);
-        client.setBottom(vBox);
         primaryStage.setTitle("CreepyPasta");
         primaryStage.setScene(new Scene(client, 1000, 800));
         primaryStage.show();
